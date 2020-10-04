acd87dba8fa05b404d17e422d3b9b39a60611b4b
S S CHAURASIYA <shyamchaurasiyassc@gmail.com>
2020-10-04 14:05:00 +0530

    initial 1 commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,19 @@
+import React, {Component} from "react"
+import Aux from "../../hoc/Auxilary"
+
+class BurgerBuilder extends Component {
+  render()
+  {
+    return (
+<Aux>
+
+<div> Burger  </div>
+<div> Build control  </div>
+
+
+</Aux>
+    )
+  }
+}
+
+export default BurgerBuilder;
